b9c59c502766eb450d835da98ae0abab344eaf6d
evoulawrence <evoulawrence@gmail.com>
2022-05-04 05:20:33 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { ProductList } from "./components/product-list";
+import { Cart } from "./components/cart";
+import 'h8k-components';
+
+const title = "EL.MENDY Shop";
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        heading: "Cap - $10",
+        name: "Cap",
+        image: `/images/items/cap.png`,
+        price: 10
+    },
+    {
+        id: 2,
+        heading: "Hand Bag - $30",
+        name: "HandBag",
+        image: `/images/items/handbag.png`,
+        price: 30
+    },
+    {
+        id: 3,
+        heading: "Shirt - $30",
+        name: "Shirt",
+        image: `/images/items/shirt.png`,
+        price: 30
+    },
+    {
+        id: 4,
+        heading: "Shoes - $50",
+        name: "Shoe",
+        image: `/images/items/shoe.png`,
+        price: 50
+    },
+    {
+        id: 5,
+        heading: "Pant - $40",
+        name: "Pant",
+        image: `/images/items/pant.png`,
+        price: 40
+    },
+    {
+        id: 6,
+        heading: "Slipper - $20",
+        name: "Slipper",
+        image: `/images/items/slipper.png`,
+        price: 20
+    }
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [cartState, setCartState] = useState({
+        items: [],
+        subTotal: 0,
+        totalPrice: 0,
+        discount: 0,
+        selectedCoupon: '0'
+    })
+
+    const [couponValue, setCouponValue] = useState(0);
+
+    const handleCoupon = (e) => {
+        setCouponValue(e.target.value);
+    };
+
+    const addToCart = (product) => {
+        const cartItem = {
+            ...product,
+            quantity: 1
+        }
+
+        const cart = {
+            ...cartState,
+            items: cartState.items.concat([cartItem]),
+            subTotal: cartState.subTotal + cartItem.price,
+            discount: couponValue,
+            totalPrice: (cartState.subTotal + cartItem.price) - couponValue
+        }
+
+        setCartState(cart)
+    }
+
+    const removeFromCart = (product) => {
+        const cart = {
+            ...cartState,
+            items: cartState.items.filter(cartItem => cartItem.id !== product.id),
+            subTotal: cartState.subTotal - product.price,
+            discount: couponValue,
+            totalPrice: (cartState.subTotal - product.price) - couponValue
+        }
+
+        setCartState(cart)
+    }
+
+    return (
+        <div>
+            <h8k-navbar header={title}></h8k-navbar>
+            <div className="layout-row shop-component">
+                <ProductList products={PRODUCTS} addToCart={addToCart} removeFromCart={removeFromCart} cartItems={cartState.items} />
+                <Cart cart={cartState} coupon={handleCoupon} couponValue={couponValue} />
+            </div>
+        </div>
+    );
 }
+
 
 export default App;
---
+++ src/components/product-list/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./index.css";
+
+export function ProductList({ addToCart, removeFromCart, products, cartItems }) {
+    return (
+        <div className="layout-row wrap justify-content-center flex-70 app-product-list">
+            {products.map((product, i) => {
+                return (
+                    <section className="w-30"
+                        data-testid={'product-item-' + i}
+                        key={product.id}>
+                        <div className="card ma-16">
+                            <img alt="Your Cart" src={product.image}
+                                className="d-inline-block align-top product-image" />
+                            <div className="card-text pa-4">
+                                <h5 className="ma-0 text-center">{product.name}</h5>
+                                <p className="ma-0 mt-8 text-center">${product.price}</p>
+                            </div>
+                            <div className="card-actions justify-content-center pa-4">
+                                {!cartItems.some((p) => p.id === product.id) ? (
+                                    <button className="x-small outlined" data-testid="btn-item-add" onClick={() => addToCart(product)}>
+                                        Add To Cart
+                                    </button>
+                                ) : (
+                                    <button className="x-small danger" data-testid="btn-item-remove" onClick={() => removeFromCart(product)}>
+                                        Remove
+                                    </button>
+                                )}
+                            </div>
+                        </div>
+                    </section>
+                )
+            })}
+        </div>
+    );
+}
+
